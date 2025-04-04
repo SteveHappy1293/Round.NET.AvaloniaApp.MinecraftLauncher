91389981e4e099f3d834a063bea8e19a6ef68be2
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-04-05 00:04:50 +0800

    添加插件打包工具

---
+++ RMCLPlugTools/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RMCLPlugTools;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
