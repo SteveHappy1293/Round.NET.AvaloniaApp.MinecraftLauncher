e277abddfefbdc5cf782c3b2fabf922c85f0e027
Happy Steve <SteveHappy1293@outlook.com>
2025-04-20 20:02:14 +0800

    存档插件 By Steve UwU

--- LevelManager/Views/Pages/LevelManage.axaml.cs
+++ LevelManager/Views/Pages/LevelManage.axaml.cs
@@ -55,127 +55,132 @@ public partial class LevelManage : UserControl
             {
                 if (Directory.Exists(path))
                 {
-
                     foreach (var ver in Directory.GetDirectories(path))
                     {
-                        var nbt = new NbtFile();
-                        nbt.LoadFromFile($"{ver}\\level.dat");
-                        NbtCompound root = nbt.RootTag;
-                        NbtCompound data = root.Get<NbtCompound>("Data");
-                        var versionName = data.Get<NbtString>("LevelName").Value;
+                        if (File.Exists($"{ver}\\level.dat"))
+                        {
+                            var nbt = new NbtFile();
+                            nbt.LoadFromFile($"{ver}\\level.dat");
+                            NbtCompound root = nbt.RootTag;
+                            NbtCompound data = root.Get<NbtCompound>("Data");
+                            var versionName = data.Get<NbtString>("LevelName").Value;
 
-                        var delete = new Button()
-                        {
-                            Content = new FluentIcon()
+                            var delete = new Button()
                             {
-                                Icon = FluentIconSymbol.Delete20Regular,
-                                Margin = new Thickness(-10),
-                            },
-                            Margin = new Thickness(5),
-                            Height = 32,
-                            Width = 32
-                        };
-                        delete.Click += (_, __) =>
-                        {
-                            Microsoft.VisualBasic.FileIO.FileSystem.DeleteDirectory(ver, UIOption.AllDialogs,
-                                RecycleOption.SendToRecycleBin, UICancelOption.DoNothing);
-                            UpdateSavesList(paths);
-                        };
-                        var launch = new Button()
-                        {
-                            Content = $"启动游戏",
-                            Margin = new Thickness(5),
-                            Height = 32,
-                            Width = 150
-                        };
-                        launch.Click += (_, __) =>
-                        {
-                            var dow = new LaunchJavaEdtion();
-                            dow.Dir = Path.Combine(path, "..","..","..");
-                            dow.Version = new DirectoryInfo(Path.Combine(path,"..")).Name;
-                            dow.Tuid = SystemMessageTaskMange.AddTask(dow);
-                            dow.Launch();
-                        };
-                        var sett = new Button()
-                        {
-                            Content = new FluentIcon()
-                            {
-                                Icon = FluentIconSymbol.Settings20Regular,
-                                Margin = new Thickness(-10),
-                            },
-                            Margin = new Thickness(5),
-                            Height = 32,
-                            Width = 32
-                        };
-                        sett.Click += (_, __) =>
-                        {
-                            var con = new ContentPageDialog()
-                            {
-                                Page = new LevelSetting { nbtfilepath = ver + "\\level.dat",nbt = nbt},
-                                Title = $"存档设置 - {versionName}",
-                            };
-                            con.Show();
-                        };
-                        var title = new Label()
-                        {
-                            Content = versionName,
-                            HorizontalContentAlignment = HorizontalAlignment.Left,
-                            VerticalContentAlignment = VerticalAlignment.Top,
-                            Margin = new Thickness(5),
-                            FontSize = 20,
-                        };
-                        list.Items.Add(new ListBoxItem()
-                        {
-                            Content = new Grid()
-                            {
-                                Height = 65,
-                                Children =
+                                Content = new FluentIcon()
                                 {
-                                    new DockPanel()
+                                    Icon = FluentIconSymbol.Delete20Regular,
+                                    Margin = new Thickness(-10),
+                                },
+                                Margin = new Thickness(5),
+                                Height = 32,
+                                Width = 32
+                            };
+                            delete.Click += (_, __) =>
+                            {
+                                Microsoft.VisualBasic.FileIO.FileSystem.DeleteDirectory(ver, UIOption.AllDialogs,
+                                    RecycleOption.SendToRecycleBin, UICancelOption.DoNothing);
+                                UpdateSavesList(paths);
+                            };
+                            var launch = new Button()
+                            {
+                                Content = $"启动游戏",
+                                Margin = new Thickness(5),
+                                Height = 32,
+                                Width = 150
+                            };
+                            launch.Click += (_, __) =>
+                            {
+                                var dow = new LaunchJavaEdtion();
+                                dow.Dir = Path.Combine(path, "..", "..", "..");
+                                dow.Version = new DirectoryInfo(Path.Combine(path, "..")).Name;
+                                dow.Tuid = SystemMessageTaskMange.AddTask(dow);
+                                dow.Launch();
+                            };
+                            var sett = new Button()
+                            {
+                                Content = new FluentIcon()
+                                {
+                                    Icon = FluentIconSymbol.Settings20Regular,
+                                    Margin = new Thickness(-10),
+                                },
+                                Margin = new Thickness(5),
+                                Height = 32,
+                                Width = 32
+                            };
+                            sett.Click += (_, __) =>
+                            {
+                                var con = new ContentPageDialog()
+                                {
+                                    Page = new LevelSetting { nbtfilepath = ver + "\\level.dat", nbt = nbt },
+                                    Title = $"存档设置 - {versionName}",
+                                };
+                                con.Show();
+                            };
+                            var title = new Label()
+                            {
+                                Content = versionName,
+                                HorizontalContentAlignment = HorizontalAlignment.Left,
+                                VerticalContentAlignment = VerticalAlignment.Top,
+                                Margin = new Thickness(5),
+                                FontSize = 20,
+                            };
+                            list.Items.Add(new ListBoxItem()
+                            {
+                                Content = new Grid()
+                                {
+                                    Height = 65,
+                                    Children =
                                     {
-                                        HorizontalAlignment = HorizontalAlignment.Left,
-                                        Children =
+                                        new DockPanel()
                                         {
-                                            new Image()
+                                            HorizontalAlignment = HorizontalAlignment.Left,
+                                            Children =
                                             {
-                                                Height = 48,
-                                                Width = File.Exists($"{ver}\\icon.png")?48 : 0,
-                                                Source = File.Exists($"{ver}\\icon.png")?new  Bitmap($"{ver}\\icon.png") : null,
-                                                VerticalAlignment = VerticalAlignment.Center
-                                            },
-                                            new Grid()
-                                            {
-                                                Children =
+                                                new Image()
                                                 {
-                                                    title,
-                                                    new Label()
+                                                    Height = 48,
+                                                    Width = File.Exists($"{ver}\\icon.png") ? 48 : 0,
+                                                    Source = File.Exists($"{ver}\\icon.png")
+                                                        ? new Bitmap($"{ver}\\icon.png")
+                                                        : null,
+                                                    VerticalAlignment = VerticalAlignment.Center
+                                                },
+                                                new Grid()
+                                                {
+                                                    Children =
                                                     {
-                                                        Content = $"创建时间：{new DirectoryInfo(ver).CreationTime}，难度：{data.Get<NbtInt>("GameType").Value}",
-                                                        HorizontalContentAlignment = HorizontalAlignment.Left,
-                                                        VerticalContentAlignment = VerticalAlignment.Bottom,
-                                                        Margin = new Thickness(5),
-                                                        FontSize = 15,
-                                                        Foreground = Brushes.LightGray,
-                                                    },
+                                                        title,
+                                                        new Label()
+                                                        {
+                                                            Content =
+                                                                $"创建时间：{new DirectoryInfo(ver).CreationTime}，难度：{data.Get<NbtInt>("GameType").Value}",
+                                                            HorizontalContentAlignment = HorizontalAlignment.Left,
+                                                            VerticalContentAlignment = VerticalAlignment.Bottom,
+                                                            Margin = new Thickness(5),
+                                                            FontSize = 15,
+                                                            Foreground = Brushes.LightGray,
+                                                        },
+                                                    }
                                                 }
                                             }
-                                        }
-                                    },
+                                        },
 
-                                    new DockPanel()
-                                    {
-                                        HorizontalAlignment = HorizontalAlignment.Right,
-                                        VerticalAlignment = VerticalAlignment.Center,
-                                        Children =
+                                        new DockPanel()
                                         {
-                                            launch,
-                                            sett,
-                                            delete
+                                            HorizontalAlignment = HorizontalAlignment.Right,
+                                            VerticalAlignment = VerticalAlignment.Center,
+                                            Children =
+                                            {
+                                                launch,
+                                                sett,
+                                                delete
+                                            }
                                         }
                                     }
-                                }
-                            },
-                        });
+                                },
+                            });
+                        }
                     }
 
                     if (list.Items.Count > 0)
