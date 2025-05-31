880aeba8c37481ac46474f37f3c99705c6e2825f
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-05-31 21:49:14 +0800

    ✨ 添加滚动视图的平滑滚动！！！（终于实现了，[放烟花]）

--- src/RMCL/RMCL/Views/Pages/Main/DownloadPages/DownloadCurseForgeAssets.axaml.cs
+++ src/RMCL/RMCL/Views/Pages/Main/DownloadPages/DownloadCurseForgeAssets.axaml.cs
@@ -1,6 +1,10 @@
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using OverrideLauncher.Core.Modules.Classes.Download.Assets.CurseForge;
+using OverrideLauncher.Core.Modules.Entry.DownloadEntry.DownloadAssetsEntry;
 
 namespace RMCL.Views.Pages.Main.DownloadPages;
 
@@ -9,5 +13,22 @@ public partial class DownloadCurseForgeAssets : UserControl
     public DownloadCurseForgeAssets()
     {
         InitializeComponent();
+    }
+
+    private void SearchBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var name = SearchBox.Text;
+
+        Task.Run(() =>
+        {
+            var result = CurseForgeSearch.Search(new CurseForgeSearchInfo()
+            {
+                ApiKey = "$2a$10$Awb53b9gSOIJJkdV3Zrgp.CyFP.dI13QKbWn/4UZI4G4ff18WneB6",
+                SearchName = name,
+                GameVersion = "",
+                Index = 0,
+                PageSize = 2
+            }).Result;
+        });
     }
 }
