4c12c4d6adb802039a07a741de2160db3f758eed
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-06-07 18:28:38 +0800

    😎 修改部分代码，添加 x86 测试构建 发布通道：RMCL.Transcend

--- src/RMCL/RMCL.Desktop/Program.cs
+++ src/RMCL/RMCL.Desktop/Program.cs
@@ -3,12 +3,13 @@ using RMCL;
 using RMCL.Config;
 using RMCL.JavaManager;
 using RMCL.Logger;
+using RMCL.PathsDictionary;
 
 sealed class Program
 {
     public static void Main(string[] args)
     {
-        var redirector = new ConsoleRedirector(Path.GetFullPath($"../RMCL/RMCL.Logs/[RMCL.Logger] {DateTime.Now.ToString("yyyy.MM.dd HHmmss.fff")}.log"));
+        var redirector = new ConsoleRedirector(Path.GetFullPath(Path.Combine(PathDictionary.LogsPath,$"[RMCL.Logger] {DateTime.Now.ToString("yyyy.MM.dd HHmmss.fff")}.log")));
         
         Console.WriteLine("Program Starting!");
         Config.LoadConfig();
--- src/RMCL/RMCL.PathsDictionary/PathDictionary.cs
+++ src/RMCL/RMCL.PathsDictionary/PathDictionary.cs
@@ -2,7 +2,8 @@ namespace RMCL.PathsDictionary;
 
 public class PathDictionary
 {
-    public const string ConfigPath = "../RMCL/RMCL.Config/Config.json";
-    public const string JavaConfigPath = "../RMCL/RMCL.Config/Java.json";
-    public const string DefaultGameFolder = "../RMCL/RMCL.Minecraft";
+    public const string ConfigPath = "RMCL/RMCL.Config/Config.json";
+    public const string JavaConfigPath = "RMCL/RMCL.Config/Java.json";
+    public const string DefaultGameFolder = "RMCL/RMCL.Minecraft";
+    public const string LogsPath = "RMCL/RMCL.Logs";
 }
