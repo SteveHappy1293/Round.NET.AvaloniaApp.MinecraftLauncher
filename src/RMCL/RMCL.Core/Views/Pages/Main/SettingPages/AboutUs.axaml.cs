a19c7bff243506feb35797c85a0e7c4b9d9386f6
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-07-14 19:58:25 +0800

    feat: 添加小玩野

--- src/RMCL/RMCL.Core/Views/Pages/Main/SettingPages/AboutUs.axaml.cs
+++ src/RMCL/RMCL.Core/Views/Pages/Main/SettingPages/AboutUs.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Controls;
 
 namespace RMCL.Core.Views.Pages.Main.SettingPages;
 
@@ -11,5 +13,21 @@ public partial class AboutUs : UserControl
         InitializeComponent();
         
         VersionBox.Text = $"{App.Current.GetType().Assembly.GetName().Version}";
+    }
+
+    private int num = 0;
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        num++;
+        if (num == 10)
+        {
+            num = 0;
+            new ContentDialog()
+            {
+                Title = "小彩蛋",
+                Content = "其实 一角钱 经常把 YangSpring429 叫做 YangWei429 (阳痿429)，恭喜你发现了这个彩蛋",
+                CloseButtonText = "确定"
+            }.ShowAsync();
+        }
     }
 }
