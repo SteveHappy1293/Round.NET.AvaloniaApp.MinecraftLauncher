9a0cc68da9ce6ea0807691ea66d6f8fe137a3a83
a ahadda <ahadda12022@outlook.com>
2025-07-19 12:39:40 +0800

    feat: 完成日志管理 100%

---
+++ src/RMCL/RMCL.Controls/Item/Safe/ExceptionItem.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RMCL.Controls.Item.Safe;
+
+public partial class ExceptionItem : UserControl
+{
+    public ExceptionItem()
+    {
+        InitializeComponent();
+    }
+}
--- src/RMCL/RMCL.Core/Views/Pages/ChildFramePage/Safe/ManageLogsChildPage.axaml.cs
+++ src/RMCL/RMCL.Core/Views/Pages/ChildFramePage/Safe/ManageLogsChildPage.axaml.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using Avalonia.Threading;
 using RMCL.Controls.Item.Safe;
+using RMCL.Core.Models.Classes.Manager.UserManager;
 using RMCL.PathsDictionary;
 
 namespace RMCL.Core.Views.Pages.ChildFramePage.Safe;
@@ -37,6 +39,34 @@ public partial class ManageLogsChildPage : UserControl
                 {
                     var it = new LogItem();
                     it.OnOpen += (sender, s) => SystemHelper.SystemHelper.OpenFile(s);
+                    it.OnSave += async (sender, s) =>
+                    {
+                        // 获取当前窗口（TopLevel）
+                        var topLevel = TopLevel.GetTopLevel(this); // 假设在 Window/UserControl 内部
+                        if (topLevel == null)
+                            return;
+
+                        // 创建 SaveFileDialog 选项
+                        var options = new FilePickerSaveOptions
+                        {
+                            Title = "保存 RMCL 日志文件",
+                            DefaultExtension = ".log",
+                            FileTypeChoices = new[]
+                            {
+                                new FilePickerFileType("日志文件") { Patterns = new[] { "*.log" } },
+                            }
+                        };
+                        options.SuggestedFileName = Path.GetFileName(s);
+                        var file = await topLevel.StorageProvider.SaveFilePickerAsync(options);
+
+                        if (file == null)
+                            return; // 用户取消
+
+                        // 获取文件路径
+                        var filePath = file.Path.LocalPath;
+
+                        File.Copy(s,filePath);
+                    };
                     LogsView.Children.Add(it);
                     it.Load(x, Path.GetFileName(x) != Path.GetFileName(RMCL.Logger.ConsoleRedirector.FileName));
                 });
